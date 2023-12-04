75b129b38d508a055ad7e3c1757d28bab23a7b03
Gergo <pancho.puskas@protonmail.com>
2023-12-04 11:09:18 +0100

    finish day 4

---
+++ day04.js
@@ -0,0 +1,58 @@
+const readFrom = require("./read-input");
+
+const args = process.argv;
+const input = readFrom(args);
+if (!input) return;
+
+const lines = input.split("\n");
+
+const partOneResult = lines.reduce((sum, line) => {
+    const [winningRaw, numbersRaw] = line.split(" | ");
+    const winningNumbers = winningRaw
+        .split(": ")[1]
+        .split(" ")
+        .map((num) => num.trim());
+
+    const points = numbersRaw.split(" ").reduce((points, number) => {
+        if (number !== "" && winningNumbers.includes(number.trim())) {
+            return points > 0 ? points + points : 1;
+        }
+
+        return points;
+    }, 0);
+
+    return sum + points;
+}, 0);
+
+console.log(partOneResult);
+
+const partTwoResult = lines
+    .reduce(
+        (copies, line, lineI) => {
+            const [winningRaw, numbersRaw] = line.split(" | ");
+            const winningNumbers = winningRaw
+                .split(": ")[1]
+                .split(" ")
+                .map((num) => num.trim());
+
+            const points = numbersRaw.split(" ").reduce((points, number) => {
+                if (number !== "" && winningNumbers.includes(number.trim())) {
+                    return points + 1;
+                }
+
+                return points;
+            }, 0);
+
+            for (let i = 0; i < copies[lineI]; i++) {
+                for (let i = 0; i < points; i++) {
+                    copies[lineI + i + 1] += 1;
+                }
+            }
+
+            return copies;
+        },
+        [...Array(lines.length)].map((_) => 1)
+    )
+    .reduce((sum, copy) => sum + copy);
+
+console.log(partTwoResult);
